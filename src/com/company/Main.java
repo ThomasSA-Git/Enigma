865b1148c966e619e42e4183d43e7e5ef7bded05
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-02-28 20:48:54 +0100

    Prototype - de mindste dele

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,34 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public int numericValue(char letter) {
+    if (letter == ' ') {
+      int num = 0;
+      return num;
+    } else {
+      int num = Character.getNumericValue(letter) - 9;
+      return num;
     }
+  }
+
+  public int numericValue2(char c) {
+    if (c == ' ') {
+      int num = 0;
+      return num;
+    } else {
+      int num = c - 'A' + 1;
+      return num;
+    }
+  }
+
+
+  public static void main(String[] args) {
+    Main obj = new Main();
+    char letter = 'A';
+    char c = 'A';
+
+    obj.numericValue(letter);
+    obj.numericValue2(c);
+
+  }
 }
