5714466b79a9602f4aea91e3151e69fb57f0738b
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-01 10:28:54 +0100

    Prototype - de mindste dele

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import java.lang.reflect.Array;
+
 public class Main {
 
-  public int numericValue(char letter) {
+  /*public int numericValue(char letter) {
     if (letter == ' ') {
       int num = 0;
       return num;
@@ -10,14 +12,26 @@ public class Main {
       int num = Character.getNumericValue(letter) - 9;  //Den numeriske værdi af A er 10, derfor -9 for at få 1.
       return num;
     }
-  }
+  }*/
 
-  public int numericValue2(char c) {
+  /*public Array charToString(String krypt){
+
+
+    char[] ch = krypt.toCharArray();
+
+    for (int i = 0; i < krypt.length(); i++) {
+      ch[i] = krypt.charAt(i);
+    }
+
+  }*/
+
+  public int getNumericValue(char c) {
+
     if (c == ' ') {
       int num = 0;
       return num;
     } else {
-      int num = c - 'A' + 1;    //A har her værdien 68. Derfor trækkes A fra hver gang, og der plusses med 1 for at få den rigtige værdi.
+      int num = c - 'A' + 1;    //A har her værdien 65. Derfor trækkes A fra hver gang, og der plusses med 1 for at få den rigtige værdi.
       return num;
     }
   }
@@ -33,12 +47,14 @@ public class Main {
 
   public static void main(String[] args) {
     Main obj = new Main();
-    char letter = 'A';
-    char c = 'A';
+    //char letter = 'A';
+    char c = 'C';
     int numLetter = 3;
+    String krypt = "Dette er en test";
 
-    obj.numericValue(letter);
-    obj.numericValue2(c);
+    //obj.numericValue(letter);
+    System.out.println(obj.getNumericValue(c));
     System.out.println(obj.returnLetter(numLetter));
+    //System.out.println(obj.charToString(krypt));
   }
 }
